9e1d74795bf0468119631242661140c815876651
MOHITH2511 <mohithragul25@gmail.com>
2026-02-12 09:17:02 +0530

    Project structure

---
+++ Backend/src/main/java/com/projectrepo/projectreposystem/ProjectRepoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.projectrepo.projectreposystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRepoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRepoSystemApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/projectrepo/projectreposystem/ProjectRepoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectrepo.projectreposystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRepoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
